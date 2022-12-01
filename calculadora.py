dbd521c3bc407e5b71f169789f55842fa3a2902d
Srglowstone <108499112+Srglowstone@users.noreply.github.com>
2022-12-01 19:34:51 +0100

    calc.py

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+print("Bienvenido a mi conversor de farenheit")
+Farenheit = int(input("introduce la temperatura: "))
+math = (Farenheit-32)*5/9
+print(f"La temperatura en celsius es de: {math}")
+print("By srglowstone")
+#by srglowstone
+
+
+
+
